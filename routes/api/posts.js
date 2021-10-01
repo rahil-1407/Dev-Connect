e9e6ba9aae7b238d7fbaf09147fcc6e33be3e146
rahil-1407 <rshzb14.7@gmail.com>
2021-10-01 20:48:25 +0530

    built PUT route to unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const auth = require('../../middleware/auth')
 const Post = require('../../models/Post')
 const User = require('../../models/User')
 const Profile = require('../../models/Profile')
+const { post } = require('request')
 
 // @route    POST api/posts
 // @desc     Create a post
@@ -119,6 +120,37 @@ router.put('/like/:id', auth, async (req, res) => {
     }
 
     post.likes.unshift({ user: req.user.id })
+
+    await post.save()
+
+    return res.json(post.likes)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route    PUT api/posts/unlike/:id
+// @desc     Unlike a post
+// @access   Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // Check if the post has not yet been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' })
+    }
+
+    //Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.params.id)
+
+    post.likes.splice(removeIndex, 1)
 
     await post.save()
 
